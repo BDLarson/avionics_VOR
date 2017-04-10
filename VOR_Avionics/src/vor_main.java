0b679d9c3558f9a54c48d780e41854f1083961d2
Blake Larson <blake7@hawaii.edu>
2017-04-09 21:33:51 -1000

    Refactored most adding in deflection angle and removing random generators.

--- VOR_Avionics/src/vor_main.java
+++ VOR_Avionics/src/vor_main.java
@@ -2,15 +2,17 @@ import java.util.*;
 
 public class vor_main
 {
-	public static int obsInput;
+	public int obsInput;
+	public static int interRadial;
+	public static int desiredRadial;
+	public static int diffAngle;
+	public static int deflection;
+	public static String bearing;
+	public static boolean toFrom;
 	public static String direction;
-	public static int planeLat;
-	public static int planeLon;
-	public static int stationLat;
-	public static int stationLon;
-	public static String toFrom;
 	public static String signal;
-	public static String quadrant;
+	
+	static Scanner userInput = new Scanner(System.in);
 
 	public static void main(String[] args)
 	{
@@ -21,265 +23,189 @@ public class vor_main
 		System.out.println("+-----------------------------------------------------+");
 		System.out.println();
 		
-		vor_main();
+		mainLoop();
 	}
-
-	private static void vor_main()
-	{
-		setOBS();
-		setDirection();
-		setPlaneLat();
-		setPlaneLon();
-		setStationLat();
-		setStationLon();
-		signalStrength(planeLat, planeLon, stationLat, stationLon);
+	
+	public static void mainLoop() {
+		setInterRadial();				//Set the Intercepted Radial
+		setDesiredRadial();				//Set the Desired Radial
+		setDeflection();				//Set the Deflection Angle
+		setToFrom();					//Set the To/From
+		setSignal();					//Set the Signal
 		
-		System.out.println("Plane Latitude: " + planeLat);
-		System.out.println("Plane Longitude: " + planeLon);
-		System.out.println("Station Latitude: " + stationLat);
-		System.out.println("Station Longitude: " + stationLon);
-		toFrom(planeLat, planeLon, stationLat, stationLon);
-		quadrant(planeLat, planeLon, stationLat, stationLon);
+		if(signal.equals("BAD")) {
+			System.out.println("The Signal is: " + getSignal());
+		} else {
+			System.out.println("The Signal is: " + getSignal() +
+							   "\nThe Station is: " + Math.abs(deflection) + 
+							   " degrees to the " + bearing +
+							   "\nMoving: " + direction + " the station");
+		}
+		programLoop();					//Check whether to loop program
 	}
 
-	//Sets the radial of the aircraft in which to move
-	public static void setOBS()
-	{
-		Scanner reader = new Scanner(System.in);
-		boolean obsBoolean = false;
+	public static void setInterRadial() {
+		boolean validInput = false;
 
-		while(obsBoolean == false)
-		{
+		while(!validInput) {
+			System.out.println("Welcome pilot!");
+			System.out.println("What is the station's intercepted radial?");
+			interRadial = userInput.nextInt();
+			if(interRadial <= 359) {
+				validInput = true;
+			} else {
+				System.out.println("Invalid radial! ");
+				System.out.println("Please enter a correct radial up to 359.");
+			}
+		}
+		System.out.println("The intercepted radial is set to: " + getInterRadial());	
+	}
+	
+	public static int getInterRadial() {
+		return interRadial;
+	}
+		
+	public static void setDesiredRadial() {
+		boolean validInput = false;
+		while(!validInput) {
+			System.out.println("What is the desired radial?");
+			desiredRadial = userInput.nextInt();
+			if(desiredRadial <= 359 && desiredRadial >= 0) {
+				validInput = true;
+			} else {
+				System.out.println("Invalid radial! ");
+				System.out.println("Please enter a radial between 0-359");
+			}
+		}
+		System.out.println("The desired radial is set to: " + getDesiredRadial());	
+	}
+	
+	public static int getDesiredRadial() {
+		return desiredRadial;
+	}
+	
+	public static void setToFrom() {
+		boolean validInput = false;
+		String decision;
+		while(!validInput) {
+			System.out.println("Is the plane moving in the direction of the station?");
+			System.out.println("Please enter 'yes' or 'no'.");
 			try {
-				System.out.print("Please enter a value for the OBS: ");
-				obsInput = reader.nextInt();
-				if (obsInput <= 360 && obsInput >= 0)
-				{
-					obsBoolean = true;
+				decision = userInput.next();
+				if (decision.equalsIgnoreCase("yes") || decision.equalsIgnoreCase("no")) {
+					if (decision.equalsIgnoreCase("yes")) {
+						toFrom = true;
+					}
+					if (decision.equalsIgnoreCase("no")) {
+						toFrom = false;
+					}
+					validInput = true;
+				} else {
+					System.out.println("Invalid response! Please enter 'yes' or 'no'.");
+					setToFrom();
 				}
-				else
-				{
-					System.out.println("Please enter a valid radial integer!");
-					System.out.println("Valid OBS is between 0-360 degrees");
-					obsBoolean = false;
-				}
-			}
-			catch (InputMismatchException er)
-			{
-				System.out.println("Please enter an integer value!");
-				reader.next();
-			}
-		}
-
-		System.out.println("The OBS is set to: " + obsInput);
-	}
-
-	//Gets the radial of the aircraft in which to move
-	public int getOBS()
-	{
-		return (obsInput);
-	}
-
-	public static void setDirection()
-	{
-		Scanner reader = new Scanner(System.in);
-		boolean dirBoolean = false;
-		while(dirBoolean == false)
-		{
-			System.out.print("Please enter the direction of the aircraft: ");
-			direction = reader.nextLine();
-			if (direction.equalsIgnoreCase("North") ||
-				direction.equalsIgnoreCase("South") ||
-				direction.equalsIgnoreCase("East") ||
-				direction.equalsIgnoreCase("West"))
-			{
-				dirBoolean = true;
-			}
-			else
-			{
-				System.out.println("Please enter a valid direction!");
-				System.out.println("North, West, East, South");
-				System.out.println("-West or -East");
-				dirBoolean = false;
+			} catch (InputMismatchException er) {
+				System.out.println("Please enter a valid response string!");
+				userInput.next();
 			}
 		}
-		System.out.println("The direction is set to: " + direction);
+		System.out.println("The direction towards the station is set to: " + getToFrom());
+		setDirection();
 	}
-
-	public String getDirection()
-	{
-		return (direction);
+	
+	public static boolean getToFrom() {
+		return toFrom;
 	}
-
-	public static void setPlaneLat()
-	{
-		Random generate = new Random();
-		planeLat = generate.nextInt(180) - 90;
-	}
-
-	public int getPlaneLat()
-	{
-		return (planeLat);
-	}
-
-	public static void setPlaneLon()
-	{
-		Random generate = new Random();
-		planeLon = generate.nextInt(360) - 180;
-
-	}
-
-	public int getPlaneLon()
-	{
-		return (planeLon);
-	}
-
-	public static void setStationLat()
-	{
-		Random generate = new Random();
-		stationLat = generate.nextInt(180) - 90;
-
-	}
-
-	public int getStationLat()
-	{
-		return (stationLat);
-	}
-
-	public static void setStationLon()
-	{
-		Random generate = new Random();
-		stationLon = generate.nextInt(360) - 180;
-	}
-
-	public int getStationLon()
-	{
-		return (stationLon);
-	}
-
-	public static void toFrom(int pLat, int pLon, int sLat, int sLon)
-	{
-		if (direction.equalsIgnoreCase("North") || direction.equalsIgnoreCase("South"))
-		{
-			if (pLon == sLon)
-			{
-				//Direct 90 degree radial from the station
-				toFrom = "ERROR the aircraft is abeam the station.";
-			}
-			else if (pLat < sLat)
-			{
-				if (direction.equalsIgnoreCase("North"))
-				{
-					toFrom = "TO";
-				}
-				else if (direction.equalsIgnoreCase("South"))
-				{
-					toFrom = "FROM";
-				}
-			}
-			else if (pLat > sLat)
-			{
-				if (direction.equalsIgnoreCase("North"))
-				{
-					toFrom = "FROM";
-				}
-				else if (direction.equalsIgnoreCase("South"))
-				{
-					toFrom = "TO";
-				}
-			}
-		}
-		else
-		{
-			if (pLon < sLon)
-			{
-				if (direction.equalsIgnoreCase("East"))
-				{
-					toFrom = "TO";
-				}
-				else if (direction.equalsIgnoreCase("West"))
-				{
-					toFrom = "FROM";
-				}
-			}
-			else if (pLon > sLon)
-			{
-				if (direction.equalsIgnoreCase("East"))
-				{
-					toFrom = "FROM";
-				}
-				else if (direction.equalsIgnoreCase("West"))
-				{
-					toFrom = "TO";
-				}
-			}
-			else
-			{
-				toFrom = "FROM";
-			}
+	
+	public static void setDirection() {
+		if(deflection == 90) {
+			direction = "ABEAM";
+		} else if(deflection < 90) {
+			direction = "TO";
+		} else {
+			direction = "FROM";
 		}
 		
-		System.out.println("The aircraft is currently moving " + toFrom + " the station");
+		/*if(!toFrom) {
+			direction = "FROM";
+		} else {
+			direction = "TO";
+		}*/
 	}
-
-	public static void signalStrength(int pLat, int pLon, int sLat, int sLon)
-	{
-		if (pLat != sLat && pLon != sLon)
-		{
-			signal = "GOOD";
-		}
-		else if (pLat == sLat || pLon == sLon)
-		{
-			//Check if aircraft is abeam the station
-			signal = "BAD abeam";
-		}
-		else
-		{
-			//Aircraft is directly above the station
+	
+	public static String getDirection() {
+		return direction;
+	}
+	
+	public static void setSignal() {
+		if(interRadial >= 0) {
+			if(Math.abs(deflection) >=89 && Math.abs(deflection) <= 91) {
+				//Checking for +-1 degree from 90 degree abeam the station
+				signal = "BAD";
+			} else {
+				signal = "GOOD";
+			}
+		} else {
+			//If intercepted radial is negative, the signal is bad to begin with.
 			signal = "BAD";
 		}
+	}
+	
+	public static String getSignal() {
+		return signal;
+	}
+	
+	public static void setDeflection() {
+		diffAngle = interRadial - desiredRadial;
 		
-		System.out.println("The station signal is currently: " +  signal);
+		/*
+		 * Determine the deflection angle based on the difference
+		 * between intercepted radial and desired radial.
+		 */
+		if(diffAngle > 180) {
+			deflection = diffAngle - 360;
+		}
+		else if(diffAngle < -180) {
+			deflection = diffAngle + 360;
+		} else {
+			deflection = diffAngle;
+		}
+		
+		/*
+		 * Determine which direction LEFT or Right, the station is
+		 * based on the calculated deflection angle.
+		 */
+		
+		if(deflection >= 0) {
+			bearing = "Right";
+		} else {
+			bearing = "Left";
+		}
 	}
 	
-	public static void quadrant(int pLat, int pLon, int sLat, int sLon)
-	{
-		if (pLon < sLon)
-		{
-			if (pLat == sLat)
-			{
-				quadrant = "LEFT DIRECT";
-			}
-			else if (pLat < sLat)
-			{
-				//Bottom Right
-				quadrant = "LEFT TO";
-			}
-			else if (pLat > sLat) 
-			{
-				quadrant = "LEFT FROM";
-			}
-		}
-		else if (pLon > sLon)
-		{
-			if (pLat == sLat)
-			{
-				quadrant = "RIGHT DIRECT";
-			}
-			else if (pLat > sLat)
-			{
-				quadrant = "RIGHT FROM";
-			}
-			else if (pLat < sLat)
-			{
-				quadrant = "RIGHT TO";
-			}
-		}
-		System.out.println("The aircraft is currently in quadrant: " + quadrant);
+	public static int getDeflection() {
+		return deflection;
 	}
 	
-	public static void calcAngle()
-	{
-		//calculate deflection angle
+	public static void programLoop() {
+		String decision;
+		System.out.println("Would you like to input a new intercepted radial?");
+		System.out.println("Please enter 'yes' or 'no'.");
+		try {
+			decision = userInput.next();
+			if(decision.equalsIgnoreCase("yes")) {
+				mainLoop();
+			}
+			else if (decision.equalsIgnoreCase("no")) {
+				System.out.println("Exitting Program!");
+			}
+			else {
+				System.out.println("Invalid response! Please enter 'yes' or 'no'.");
+				programLoop();
+			}
+		} catch (InputMismatchException er) {
+			System.out.println("Please enter a valid response string!");
+			userInput.next();
+		}
 	}
 }
---
+++ VOR_Avionics/src/vor_test.java
@@ -0,0 +1,4 @@
+
+public class vor_test {
+
+}
